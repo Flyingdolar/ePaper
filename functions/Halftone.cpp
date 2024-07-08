bc95bcaa6c2f2a8becd91473fefce0eb9016dbec
翔元 [Lab Linux] <31057818+Flyingdolar@users.noreply.github.com>
2024-07-08 19:56:31 +0800

    refactor[Halftone]: Refactor Halftone class

--- functions/Halftone.cpp
+++ functions/Halftone.cpp
@@ -2,84 +2,6 @@
 
 namespace halftone {
 
-// -- Internal Function for DBS Halftoning --
-// Update the low-pass Error Image
-cv::Mat1f _altLpErr_(const cv::Mat1f lpErrImg, std::vector<cv::Vec3i> pos, int kernelSize, const cv::Mat1f gskMat2D) {
-    int height = lpErrImg.rows, width = lpErrImg.cols, halfK = kernelSize / 2;
-    cv::Mat1f newLpErrImg = lpErrImg.clone();
-
-    for (cv::Vec3i pVal : pos)
-        for (int rdx = -halfK; rdx <= halfK; rdx++)
-            for (int cdx = -halfK; cdx <= halfK; cdx++) {
-                int nRow = pVal[0] + rdx, nCol = pVal[1] + cdx;
-                int distRow = kernelSize / 2 + rdx, distCol = kernelSize / 2 + cdx;
-                if (nRow < 0 || nRow >= height || nCol < 0 || nCol >= width) continue;
-                newLpErrImg.at<float>(nRow, nCol) += gskMat2D(distRow, distCol) * ((pVal[2] == 0) ? 1 : -1);
-            }
-    return newLpErrImg;
-}
-
-// Minimum Error Decision
-cv::Vec2i _minErrDec_(const cv::Mat1f resImg, const cv::Mat1f lpErrImg, cv::Vec2i centPos, int kernelSize, const cv::Mat1f gskMat2D) {
-    int height = resImg.rows, width = resImg.cols, halfK = kernelSize / 2;
-    float minDscErr = 0;
-    cv::Vec2i posME = {-1, -1};  // Minimum Error Position
-    int movHalfK = 1;            // Moving Half Kernel Size
-
-    // For Every Pixel in the Kernel, Calculate the Descending Error
-    for (int rdx = -movHalfK; rdx <= movHalfK; rdx++)
-        for (int cdx = -movHalfK; cdx <= movHalfK; cdx++) {
-            cv::Vec2i nbrPos = {centPos[0] + rdx, centPos[1] + cdx};
-            if (nbrPos[0] < 0 || nbrPos[0] >= height || nbrPos[1] < 0 || nbrPos[1] >= width) continue;
-            if (centPos != nbrPos)  // Skip Neighbor with same value
-                if (resImg.at<float>(nbrPos[0], nbrPos[1]) == resImg.at<float>(centPos[0], centPos[1]) && centPos != nbrPos) continue;
-            int modCentPos = (resImg.at<float>(centPos[0], centPos[1]) == 0) ? 1 : -1;
-            int modNPos = (resImg.at<float>(nbrPos[0], nbrPos[1]) == 0) ? 1 : -1;
-
-            // Collect all Error Value Altered Pixels
-            std::vector<cv::Vec6i> posVecs;  // {Row, Col, CentDistRow, CentDistCol, NbrDistRow, NbrDistCol}
-            for (int rdxAlt = -movHalfK; rdxAlt <= movHalfK; rdxAlt++)
-                for (int cdxAlt = -movHalfK; cdxAlt <= movHalfK; cdxAlt++) {
-                    cv::Vec2i posAlt = {centPos[0] + rdxAlt, centPos[1] + cdxAlt}, centDist = {rdxAlt, cdxAlt};
-                    cv::Vec2i nbrDist = {posAlt[0] - nbrPos[0], posAlt[1] - nbrPos[1]};
-                    if (posAlt[0] < 0 || posAlt[0] >= height || posAlt[1] < 0 || posAlt[1] >= width) continue;
-                    posVecs.push_back({posAlt[0], posAlt[1], centDist[0], centDist[1], nbrDist[0], nbrDist[1]});
-                    if (centDist[0] < -halfK || centDist[0] > halfK || centDist[1] < -halfK || centDist[1] > halfK)
-                        posVecs.back()[2] = -1, posVecs.back()[3] = -1;  // Skip the Position Far from the Center
-                    if (nbrDist[0] < -halfK || nbrDist[0] > halfK || nbrDist[1] < -halfK || nbrDist[1] > halfK)
-                        posVecs.back()[4] = -1, posVecs.back()[5] = -1;                      // Skip the Position Far from the Neighbor
-                    if (centPos == nbrPos) posVecs.back()[4] = -1, posVecs.back()[5] = -1;   // Skip the Same Position
-                    if (posVecs.back()[3] < 0 && posVecs.back()[5] < 0) posVecs.pop_back();  // Remove Not-Valid Position
-                }
-
-            // Calculate the Descending Error
-            float dscErr = 0;
-            for (cv::Vec6i pVal : posVecs) {
-                cv::Vec2i centDist = {pVal[2] + halfK, pVal[3] + halfK}, nbrDist = {pVal[4] + halfK, pVal[5] + halfK};
-                float deltaE = 0;
-                // First Calculate Small Delta E
-                if (centDist[0] > 0 && centDist[1] > 0) deltaE += gskMat2D(centDist[0], centDist[1]) * modCentPos;
-                if (nbrDist[0] > 0 && nbrDist[1] > 0) deltaE += gskMat2D(nbrDist[0], nbrDist[1]) * modNPos;
-                // Then Transfer to Big Delta E and get Sum
-                dscErr += std::pow(deltaE + lpErrImg.at<float>(pVal[0], pVal[1]), 2) - std::pow(lpErrImg.at<float>(pVal[0], pVal[1]), 2);
-            }
-            if (dscErr < minDscErr) minDscErr = dscErr, posME = nbrPos;  // Update the Minimum Error Position
-        }
-    return posME;
-}
-
-// Visualize Error Image
-cv::Mat3f _visErr_(cv::Mat1f errImg) {
-    cv::Mat3f visImg(errImg.size());
-    for (int row = 0; row < errImg.rows; row++)
-        for (int col = 0; col < errImg.cols; col++) {
-            float val = errImg(row, col);
-            if (val < 0) visImg(row, col) = cv::Vec3f(0, 0, -val);  // Red for Negative
-            if (val >= 0) visImg(row, col) = cv::Vec3f(val, 0, 0);  // Blue for Positive
-        }
-    return visImg;
-}
-
 // Direct Binary Search (DBS) Halftoning
 cv::Mat1f DBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters, bool verbose) {
     int height = grayImg.rows, width = grayImg.cols, halfK = kernelSize / 2;
@@ -92,7 +14,7 @@ cv::Mat1f DBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters, b
     for (int row = 0; row < height; row++)
         for (int col = 0; col < width; col++) resImg.at<float>(row, col) = (rand() % 2 == 0) ? 0 : 1;
     if (verbose) saveData::initVar(workFolder + "/Iter"), saveData::imgMat(resImg, "resImg_0");
-    if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(_visErr_(errImg), "errImg_0");
+    if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(detail::viewErr(errImg), "errImg_0");
 
     // 2. Create Gaussian Kernel
     cv::Mat1f gskMat2D = cv::Mat::zeros(kernelSize, kernelSize, CV_32F);
@@ -134,7 +56,7 @@ cv::Mat1f DBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters, b
             }
 
             // 4-1. MinError Decision
-            cv::Vec2i posME = _minErrDec_(resImg, lpErrImg, {row, col}, kernelSize, gskMat2D);
+            cv::Vec2i posME = detail::getMinEPos(resImg, lpErrImg, {row, col}, kernelSize, gskMat2D);
             if (posME[0] < 0 && posME[1] < 0) continue;  // Skip if no minimum error
             swapNum++;                                   // Increase the Swap Number if not skipped
 
@@ -144,14 +66,14 @@ cv::Mat1f DBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters, b
             for (cv::Vec3i pVal : posAlt) resImg.at<float>(pVal[0], pVal[1]) = (pVal[2] == 0) ? 1 : 0;  // Update the Result Image
 
             // 4-3. Update the low-pass Error Image
-            lpErrImg = _altLpErr_(lpErrImg, posAlt, kernelSize, gskMat2D);
+            lpErrImg = detail::altLpErr(lpErrImg, posAlt, kernelSize, gskMat2D);
         }
         // Update the Error Image
         if (verbose) errImg = resImg - grayImg;
 
         // Save the Iteration Result
         if (verbose) saveData::initVar(workFolder + "/Iter"), saveData::imgMat(resImg, "resImg_" + std::to_string(iter + 1)), swapNum = 0;
-        if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(_visErr_(errImg), "errImg_" + std::to_string(iter + 1));
+        if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(detail::viewErr(errImg), "errImg_" + std::to_string(iter + 1));
         if (verbose) saveData::initVar(workFolder + "/LpErr"), saveData::imgMat(lpErrImg, "lpErrImg_" + std::to_string(iter + 1));
         if (verbose) saveData::initVar(workFolder);
     }
@@ -170,7 +92,7 @@ cv::Mat1f RTBDBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters
         for (int col = 0; col < width; col++) resImg.at<float>(row, col) = (rand() % 2 == 0) ? 0 : 1;
     cv::Mat1f errImg = resImg - grayImg;  // Error Image
     if (verbose) saveData::initVar(workFolder + "/Iter"), saveData::imgMat(resImg, "resImg_0");
-    if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(_visErr_(errImg), "errImg_0");
+    if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(detail::viewErr(errImg), "errImg_0");
 
     // 2. Create Gaussian Kernel
     cv::Mat1f gskMat2D = cv::Mat::zeros(kernelSize, kernelSize, CV_32F);
@@ -206,7 +128,7 @@ cv::Mat1f RTBDBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters
                 }
 
                 // 4-1. MinError Decision
-                cv::Vec2i posME = _minErrDec_(resImg, lpErrImg, {row, col}, kernelSize, gskMat2D);
+                cv::Vec2i posME = detail::getMinEPos(resImg, lpErrImg, {row, col}, kernelSize, gskMat2D);
                 if (posME[0] < 0 && posME[1] < 0) continue;  // Skip if no minimum error
                 swapNum++;                                   // Increase the Swap Number if not skipped
 
@@ -216,32 +138,34 @@ cv::Mat1f RTBDBS(const cv::Mat1f grayImg, int kernelSize, float sigma, int iters
                 for (cv::Vec3i pVal : posAlt) resImg.at<float>(pVal[0], pVal[1]) = (pVal[2] == 0) ? 1 : 0;  // Update the Result Image
 
                 // 4-3. Update the low-pass Error Image
-                lpErrImg = _altLpErr_(lpErrImg, posAlt, kernelSize, gskMat2D);
+                lpErrImg = detail::altLpErr(lpErrImg, posAlt, kernelSize, gskMat2D);
             }
         // Update the Error Image
         if (verbose) errImg = resImg - grayImg;
 
         // Save the Iteration Result
         if (verbose) saveData::initVar(workFolder + "/Iter"), saveData::imgMat(resImg, "resImg_" + std::to_string(iter + 1)), swapNum = 0;
-        if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(_visErr_(errImg), "errImg_" + std::to_string(iter + 1));
-        if (verbose) saveData::initVar(workFolder + "/LpErr"), saveData::imgMat(_visErr_(lpErrImg), "lpErrImg_" + std::to_string(iter + 1));
+        if (verbose) saveData::initVar(workFolder + "/Err"), saveData::imgMat(detail::viewErr(errImg), "errImg_" + std::to_string(iter + 1));
+        if (verbose) saveData::initVar(workFolder + "/LpErr"), saveData::imgMat(detail::viewErr(lpErrImg), "lpErrImg_" + std::to_string(iter + 1));
         if (verbose) saveData::initVar(workFolder);
     }
     return resImg;
 }
 
+// Dithering Halftoning
 cv::Mat1f Dither(const cv::Mat1f grayImg, int kernelSize, bool verbose) {
     int height = grayImg.rows, width = grayImg.cols;
     cv::Mat1f resImg = grayImg.clone();
 
     // 1. Create Dither Matrix
-    std::vector<int> randList;
-    for (int idx = 0; idx < kernelSize * kernelSize; idx++) randList.push_back(idx);
-    std::random_shuffle(randList.begin(), randList.end());
-    cv::Mat1i ditherMat = cv::Mat1f::zeros(kernelSize, kernelSize);
-    for (int row = 0; row < kernelSize; row++)
-        for (int col = 0; col < kernelSize; col++) ditherMat.at<int>(row, col) = randList[row * kernelSize + col];
-    if (verbose) saveData::imgMat(ditherMat, "DitherMatrix", kernelSize * kernelSize);
+    cv::Mat1b ditherMat;
+    if (kernelSize == 2) ditherMat = tMap2;  // 2x2 Dithering Matrix
+    if (kernelSize == 4) ditherMat = tMap4;  // 4x4 Dithering Matrix
+    if (kernelSize == 8) ditherMat = tMap8;  // 8x8 Dithering Matrix
+    if (ditherMat.empty()) {
+        std::cerr << "Dithering Kernel Size is not Supported!" << std::endl;
+        return resImg;
+    }
 
     // 2. Dithering Process
     for (int row = 0; row < height; row++)
@@ -255,4 +179,88 @@ cv::Mat1f Dither(const cv::Mat1f grayImg, int kernelSize, bool verbose) {
     return resImg;
 }
 
+// Error Diffusion Halftoning
+cv::Mat1f ErrDiff(const cv::Mat1f grayImg, int kernelSize, bool verbose) {
+}
+
 }  // namespace halftone
+
+namespace halftone::detail {  // Detail Functions
+// Alter & Update the low-pass Error Image
+cv::Mat1f altLpErr(const cv::Mat1f lpErrImg, std::vector<cv::Vec3i> pos, int kernelSize, const cv::Mat1f gskMat2D) {
+    int height = lpErrImg.rows, width = lpErrImg.cols, halfK = kernelSize / 2;
+    cv::Mat1f newLpErrImg = lpErrImg.clone();
+
+    for (cv::Vec3i pVal : pos)
+        for (int rdx = -halfK; rdx <= halfK; rdx++)
+            for (int cdx = -halfK; cdx <= halfK; cdx++) {
+                int nRow = pVal[0] + rdx, nCol = pVal[1] + cdx;
+                int distRow = kernelSize / 2 + rdx, distCol = kernelSize / 2 + cdx;
+                if (nRow < 0 || nRow >= height || nCol < 0 || nCol >= width) continue;
+                newLpErrImg.at<float>(nRow, nCol) += gskMat2D(distRow, distCol) * ((pVal[2] == 0) ? 1 : -1);
+            }
+    return newLpErrImg;
+}
+
+// Choose the Minimum Error Position
+cv::Vec2i getMinEPos(const cv::Mat1f resImg, const cv::Mat1f lpErrImg, cv::Vec2i centPos, int kernelSize, const cv::Mat1f gskMat2D) {
+    int height = resImg.rows, width = resImg.cols, halfK = kernelSize / 2;
+    float minDscErr = 0;
+    cv::Vec2i posME = {-1, -1};  // Minimum Error Position
+    int movHalfK = 1;            // Moving Half Kernel Size
+
+    // For Every Pixel in the Kernel, Calculate the Descending Error
+    for (int rdx = -movHalfK; rdx <= movHalfK; rdx++)
+        for (int cdx = -movHalfK; cdx <= movHalfK; cdx++) {
+            cv::Vec2i nbrPos = {centPos[0] + rdx, centPos[1] + cdx};
+            if (nbrPos[0] < 0 || nbrPos[0] >= height || nbrPos[1] < 0 || nbrPos[1] >= width) continue;
+            if (centPos != nbrPos)  // Skip Neighbor with same value
+                if (resImg.at<float>(nbrPos[0], nbrPos[1]) == resImg.at<float>(centPos[0], centPos[1]) && centPos != nbrPos) continue;
+            int modCentPos = (resImg.at<float>(centPos[0], centPos[1]) == 0) ? 1 : -1;
+            int modNPos = (resImg.at<float>(nbrPos[0], nbrPos[1]) == 0) ? 1 : -1;
+
+            // Collect all Error Value Altered Pixels
+            std::vector<cv::Vec6i> posVecs;  // {Row, Col, Distance to Center(Row, Col), Distance to Neighbor(Row, Col) }
+            for (int rdxAlt = -movHalfK; rdxAlt <= movHalfK; rdxAlt++)
+                for (int cdxAlt = -movHalfK; cdxAlt <= movHalfK; cdxAlt++) {
+                    cv::Vec2i posAlt = {centPos[0] + rdxAlt, centPos[1] + cdxAlt}, centDist = {rdxAlt, cdxAlt};
+                    cv::Vec2i nbrDist = {posAlt[0] - nbrPos[0], posAlt[1] - nbrPos[1]};
+                    if (posAlt[0] < 0 || posAlt[0] >= height || posAlt[1] < 0 || posAlt[1] >= width) continue;
+                    posVecs.push_back({posAlt[0], posAlt[1], centDist[0], centDist[1], nbrDist[0], nbrDist[1]});
+                    if (centDist[0] < -halfK || centDist[0] > halfK || centDist[1] < -halfK || centDist[1] > halfK)
+                        posVecs.back()[2] = -1, posVecs.back()[3] = -1;  // Skip the Position Far from the Center
+                    if (nbrDist[0] < -halfK || nbrDist[0] > halfK || nbrDist[1] < -halfK || nbrDist[1] > halfK)
+                        posVecs.back()[4] = -1, posVecs.back()[5] = -1;                      // Skip the Position Far from the Neighbor
+                    if (centPos == nbrPos) posVecs.back()[4] = -1, posVecs.back()[5] = -1;   // Skip the Same Position
+                    if (posVecs.back()[3] < 0 && posVecs.back()[5] < 0) posVecs.pop_back();  // Remove Not-Valid Position
+                }
+
+            // Calculate the Descending Error
+            float dscErr = 0;
+            for (cv::Vec6i pVal : posVecs) {
+                cv::Vec2i centDist = {pVal[2] + halfK, pVal[3] + halfK}, nbrDist = {pVal[4] + halfK, pVal[5] + halfK};
+                float deltaE = 0;
+                // First Calculate Small Delta E
+                if (centDist[0] > 0 && centDist[1] > 0) deltaE += gskMat2D(centDist[0], centDist[1]) * modCentPos;
+                if (nbrDist[0] > 0 && nbrDist[1] > 0) deltaE += gskMat2D(nbrDist[0], nbrDist[1]) * modNPos;
+                // Then Transfer to Big Delta E and get Sum
+                dscErr += std::pow(deltaE + lpErrImg.at<float>(pVal[0], pVal[1]), 2) - std::pow(lpErrImg.at<float>(pVal[0], pVal[1]), 2);
+            }
+            if (dscErr < minDscErr) minDscErr = dscErr, posME = nbrPos;  // Update the Minimum Error Position
+        }
+    return posME;
+}
+
+// Visualize Error Image
+cv::Mat3f viewErr(cv::Mat1f errImg) {
+    cv::Mat3f visImg(errImg.size());
+    for (int row = 0; row < errImg.rows; row++)
+        for (int col = 0; col < errImg.cols; col++) {
+            float val = errImg(row, col);
+            if (val < 0) visImg(row, col) = cv::Vec3f(0, 0, -val);  // Red for Negative
+            if (val >= 0) visImg(row, col) = cv::Vec3f(val, 0, 0);  // Blue for Positive
+        }
+    return visImg;
+}
+
+}  // namespace halftone::detail
--- include/Halftone.hpp
+++ include/Halftone.hpp
@@ -7,6 +7,18 @@
 
 namespace halftone {
 
+// Threshold Map Matrix for Dithering
+const cv::Mat1b tMap2 = (cv::Mat1b(2, 2) << 0, 2, 3, 1);
+const cv::Mat1b tMap4 = (cv::Mat1b(4, 4) << 0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5);
+const cv::Mat1b tMap8 = (cv::Mat1b(8, 8) << 0, 32, 8, 40, 2, 34, 10, 42, 48, 16, 56, 24, 50, 18, 58, 26,
+                         12, 44, 4, 36, 14, 46, 6, 38, 60, 28, 52, 20, 62, 30, 54, 22,
+                         3, 35, 11, 43, 1, 33, 9, 41, 51, 19, 59, 27, 49, 17, 57, 25,
+                         15, 47, 7, 39, 13, 45, 5, 37, 63, 31, 55, 23, 61, 29, 53, 21);
+
+// Error Diffusion Kernels
+const cv::Mat1b kFloydSteinberg = (cv::Mat1b(3, 3) << 0, 0, 7, 3, 5, 1, 0, 0, 0);
+const cv::Mat1b kJJN = (cv::Mat1b(3, 5) << 0, 0, 0, 7, 5, 3, 5, 7, 5, 3, 1, 3, 5, 3, 1);
+
 /**
  * @brief Direct Binary Search (DBS) Halftoning
  * @param img Input image (Single Channel, 0-1, float)
@@ -19,7 +31,7 @@ namespace halftone {
 cv::Mat1f DBS(const cv::Mat1f img, int kernelSize, float sigma, int iters, bool verbose = false);
 
 /**
- * @brief Random Threshold Binary Direct Binary Search (RTBDBS) Halftoning
+ * @brief Random Tiled Block Direct Binary Search (RTBDBS) Halftoning
  * @param img Input image (Single Channel, 0-1, float)
  * @param kernelSize Kernel size for DBS
  * @param sigma Sigma value for Point Spread Function (PSF)
@@ -32,11 +44,21 @@ cv::Mat1f RTBDBS(const cv::Mat1f img, int kernelSize, float sigma, int iters, bo
 /**
  * @brief Halftone by Dithering
  * @param grayImg Input image (Single Channel, 0-1, float)
- * @param kernelSize Kernel size for Dithering
+ * @param kernelSize Kernel size for Dithering (2, 4, 8)
  * @param verbose Verbose mode (default: false)
  * @return Halftoned image (Single Channel, 0-1, float)
  */
 cv::Mat1f Dither(const cv::Mat1f grayImg, int kernelSize, bool verbose = false);
+
+namespace detail {
+
+cv::Mat1f altLpErr(const cv::Mat1f lpErrImg, std::vector<cv::Vec3i> pos, int kernelSize, const cv::Mat1f gskMat2D);
+
+cv::Vec2i getMinEPos(const cv::Mat1f resImg, const cv::Mat1f lpErrImg, cv::Vec2i centPos, int kernelSize, const cv::Mat1f gskMat2D);
+
+cv::Mat3f viewErr(cv::Mat1f errImg);
+
+}  // namespace detail
 
 }  // namespace halftone
 
